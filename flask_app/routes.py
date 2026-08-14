8bb72deeae4f7913e6b1a43abafc2349ec82d2e8
Mushfique <mushfique.rahman12@gmail.com>
2026-08-14 19:13:44 -0400

    Added functions to Create, Update & Delete Posts

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,32 +1,21 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 from flask_app import app, db, bcrypt
-from flask_app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask_app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2', 
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
+# Creates the Home Page and About Page routes
 @app.route("/")
 @app.route("/home")
 def home():
+    # Queries all the posts from the database and passes them to the home.html template to be displayed
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
+# Creates the About Page route
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
@@ -141,3 +130,65 @@ def account():
 
     # Renders the account.html template with the title, image_file, and form parameters
     return render_template("account.html", title="Account", image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    # Uses the Form to extract the User Input Data
+    form = PostForm()
+    # Validates the form using the Validators defined in the PostForm class
+    if form.validate_on_submit():
+        # Creates a new Post object with the title, content, and author (current user) from the form data and adds it to the database
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+
+    # Passes the form parameter to access this form in the HTML template
+    return render_template("create_post.html", title="New Post", form=form, legend="New Post")
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    # Queries the Post object from the database using the post_id parameter and returns a 404 error if not found
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    # Checks if the current user is the author of the post. If not, aborts with a 403 error (Forbidden)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    # Validates the form using the Validators defined in the PostForm class
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        # Commits the changes to the database after updating the post's title and content
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        # Pre-populates the form fields with the current post's title and content when the page is loaded for the first time
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template("create_post.html", title="New Post", form=form, legend="Update Post")
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    # Deletes the post from the database and commits the changes
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
